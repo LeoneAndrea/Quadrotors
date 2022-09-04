0dbb4e3b364de53db0460939f52616e9fd31dc73
LeoneAndrea <106384505+LeoneAndrea@users.noreply.github.com>
2022-09-04 15:30:18 +0000

    Deploying to gh-pages from @ LeoneAndrea/Quadrotors@a379db68f223b5daafd5e6c3a8e1682cf7f738d0 🚀

--- src/control_functions/derivhatq2w.m
+++
@@ -1,8 +0,0 @@
-function u=derivhatq2w(q,w,qdot,wdot)
-% Support function used to define the control functions
-
-    % u = (q'*w)*q - (q'*q)*w
-    
-    u = (qdot'*w + q'*wdot)*q + (q'*w)*qdot - 2*(q'*qdot)*w - (q'*q)*wdot;
-
-end
